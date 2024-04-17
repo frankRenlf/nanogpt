7cba77c5045c864736ef526ac4f1e59ab85eacec
Ren LIngfeng <e1143935@u.nus.edu>
2024-04-17 11:31:31 +0800

    add cur

--- README.md
+++ README.md
@@ -1,66 +1,10 @@
-# Dual-GPT Implementations
 
-This repository contains two distinct implementations of the multi-head attention mechanism used in GPT models. The attention mechanism is a critical component that allows the model to focus on different parts of the input sequence when predicting the output.
+# nanogpt-lecture
 
-## Implementation Details
+Code created in the [Neural Networks: Zero To Hero](https://karpathy.ai/zero-to-hero.html) video lecture series, specifically on the first lecture on nanoGPT. Publishing here as a Github repo so people can easily hack it, walk through the `git log` history of it, etc.
 
-### Single-Matrix-Multiplication Multi-Head GPT (SM-GPT)
+NOTE: sadly I did not go too much into model initialization in the video lecture, but it is quite important for good performance. The current code will train and work fine, but its convergence is slower because it starts off in a not great spot in the weight space. Please see [nanoGPT model.py](https://github.com/karpathy/nanoGPT/blob/master/model.py) for `# init all weights` comment, and especially how it calls the `_init_weights` function. Even more sadly, the code in this repo is a bit different in how it names and stores the various modules, so it's not possible to directly copy paste this code here. My current plan is to publish a supplementary video lecture and cover these parts, then I will also push the exact code changes to this repo. For now I'm keeping it as is so it is almost exactly what we actually covered in the video.
 
-The `gpt_.py` contains an implementation of the GPT model that utilizes a single matrix multiplication to compute multi-head attention. This method reshapes the input tensor to allow for parallel computation of all heads within the attention mechanism. The main advantage of this approach is efficiency; by performing one large matrix multiplication instead of multiple smaller ones, we reduce computational overhead and take better advantage of hardware acceleration.
+### License
 
-Key features:
-- Efficient computation of attention with fewer sequential steps.
-- Utilization of batch matrix multiplication for parallel processing of heads.
-- Reduction in Python-level for loop overheads.
-
-### Loop-Based Multi-Head GPT (LB-GPT)
-
-The `gpt.py` directory contains an implementation of the GPT model that computes each head's attention separately within a loop and then concatenates the results. This method is more straightforward and easier to understand, as each attention head is computed one after another, which can be beneficial for debugging and educational purposes.
-
-Key features:
-- Simplicity in understanding and debugging the attention mechanism.
-- Direct control over each head's computation, allowing for custom head-level manipulations.
-- Each head's output can be independently analyzed before concatenation.
-
-# Comparison
-
-- `gpt_.py` is optimized for performance and is best suited for scenarios where computational resources and efficiency are paramount.
-- `gpt.py` is optimized for clarity and is best suited for educational purposes, debugging, or research where individual attention head behavior needs to be studied.
-
-Both implementations achieve the same theoretical result, but they differ in execution and performance characteristics.
-
-# Tokenize
-data use minbpe as the tokenizer
-
-# Trained components
-You can find:
-- trained models in model_dict dict
-- trained tokenizers in tokenizers dict
-- dataset in data dict
-- generated text in generate_text dict
-
-You can modify config:
-- config in config.py
-
-# Usage
-
-Please refer to the individual directories for detailed instructions on how to run each implementation.
-
-
-# Requirements
-
-```bash
-conda create -n gpt python=3.10 -y
-
-conda activate gpt
-
-pip install -r requirements.txt
-```
-## train or generate
-```bash
-python gpt.py # or gpt_.py
-```
-
-# Future work
-
-prepare to add rlhf module
+MIT
--- gpt_cur.py
+++ gpt_cur.py
@@ -40,65 +40,10 @@ def estimate_loss():
     return out
 
 
-# @save
-def transpose_qkv(X, num_heads):
-    """为了多注意力头的并行计算而变换形状"""
-    # 输入X的形状:(batch_size，查询或者“键－值”对的个数，num_hiddens)
-    # 输出X的形状:(batch_size，查询或者“键－值”对的个数，num_heads，
-    # num_hiddens/num_heads)
-    X = X.reshape(X.shape[0], X.shape[1], num_heads, -1)
-
-    # 输出X的形状:(batch_size，num_heads，查询或者“键－值”对的个数,
-    # num_hiddens/num_heads)
-    X = X.permute(0, 2, 1, 3)
-
-    # 最终输出的形状:(batch_size*num_heads,查询或者“键－值”对的个数,
-    # num_hiddens/num_heads)
-    return X.reshape(-1, X.shape[2], X.shape[3])
-
-
-# @save
-def transpose_output(X, num_heads):
-    """逆转transpose_qkv函数的操作"""
-    # print(X.shape)
-    # assert 1 == 2
-    X = X.reshape(-1, num_heads, X.shape[1], X.shape[2])
-    X = X.permute(0, 2, 1, 3)
-    return X.reshape(X.shape[0], X.shape[1], -1)
-
-
-def sequence_mask(X, valid_len, value=0):
-    """在序列中屏蔽不相关的项"""
-    maxlen = X.size(1)
-    # print(X.shape,valid_len.shape)
-    mask = (
-        torch.arange((maxlen), dtype=torch.float32, device=X.device)[None, :]
-        < valid_len[:, None]
-    )
-    X[~mask] = value
-    return X
-
-
-# @save
-def masked_softmax(X, valid_lens):
-    """通过在最后一个轴上掩蔽元素来执行softmax操作"""
-    # X:3D张量，valid_lens:1D或2D张量
-    if valid_lens is None:
-        return nn.functional.softmax(X, dim=-1)
-    else:
-        shape = X.shape
-        if valid_lens.dim() == 1:
-            valid_lens = torch.repeat_interleave(valid_lens, shape[1])
-        else:
-            valid_lens = valid_lens.reshape(-1)
-        # 最后一轴上被掩蔽的元素使用一个非常大的负值替换，从而其softmax输出为0
-
-        X = sequence_mask(X.reshape(-1, shape[-1]), valid_lens, value=float("-inf"))
-        # print(X.shape)
-        # print(X[0:3, :])
-        # print(X[256, :])
-        # assert 1 == 2
-        return nn.functional.softmax(X.reshape(shape), dim=-1)
+def generate_square_subsequent_mask(size):
+    """生成因果掩码，掩盖未来的位置"""
+    mask = torch.triu(torch.ones(size, size), diagonal=1)
+    return mask
 
 
 # @save
@@ -113,12 +58,16 @@ class DotProductAttention(nn.Module):
     # keys的形状：(batch_size，“键－值”对的个数，d)
     # values的形状：(batch_size，“键－值”对的个数，值的维度)
     # valid_lens的形状:(batch_size，)或者(batch_size，查询的个数)
-    def forward(self, queries, keys, values, valid_lens=None):
-        d = queries.shape[-1]
-        # 设置transpose_b=True为了交换keys的最后两个维度
-        scores = torch.bmm(queries, keys.transpose(1, 2)) / math.sqrt(d)
-        self.attention_weights = masked_softmax(scores, valid_lens)
-        return torch.bmm(self.dropout(self.attention_weights), values)
+    def forward(self, queries, keys, values, mask=None):
+        d_k = queries.shape[-1]
+        scores = torch.matmul(queries, keys.transpose(-2, -1)) / math.sqrt(d_k)
+        if mask is not None:
+            mask = mask.unsqueeze(1)
+            scores = scores.masked_fill(mask == 0, -1e9)
+        scores = F.softmax(scores, dim=-1)
+        if mode == "train":
+            scores = self.dropout(self.attention_weights)
+        return torch.bmm(scores, values)
 
 
 # @save
@@ -138,38 +87,25 @@ class MSA(nn.Module):
     ):
         super(MSA, self).__init__(**kwargs)
         self.num_heads = num_heads
+        self.num_hiddens = num_hiddens
+        self.d_k = num_hiddens // num_heads
         self.attention = DotProductAttention(dropout)
         self.W_q = nn.Linear(query_size, num_hiddens, bias=bias)
         self.W_k = nn.Linear(key_size, num_hiddens, bias=bias)
         self.W_v = nn.Linear(value_size, num_hiddens, bias=bias)
         self.W_o = nn.Linear(num_hiddens, num_hiddens, bias=bias)
 
-    def forward(self, queries, keys, values, valid_lens):
-        # queries，keys，values的形状:
-        # (batch_size，查询或者“键－值”对的个数，num_hiddens)
-        # valid_lens　的形状:
-        # (batch_size，)或(batch_size，查询的个数)
-        # 经过变换后，输出的queries，keys，values　的形状:
-        # (batch_size*num_heads，查询或者“键－值”对的个数，
-        # num_hiddens/num_heads)
-        queries = transpose_qkv(self.W_q(queries), self.num_heads)
-        keys = transpose_qkv(self.W_k(keys), self.num_heads)
-        values = transpose_qkv(self.W_v(values), self.num_heads)
-        if valid_lens is not None:
-            # 在轴0，将第一项（标量或者矢量）复制num_heads次，
-            # 然后如此复制第二项，然后诸如此类。
-            valid_lens = torch.repeat_interleave(
-                valid_lens, repeats=self.num_heads, dim=0
-            )
-        # output的形状:(batch_size*num_heads，查询的个数，
-        # num_hiddens/num_heads)
-        output = self.attention(queries, keys, values, valid_lens)
+    def forward(self, queries, keys, values, mask):
 
-        # assert 1 == 2
+        bs = queries.size(0)
+        queries = self.W_q(queries).view(bs, -1, self.num_heads, self.d_k)
+        keys = self.W_k(keys).view(bs, -1, self.num_heads, self.d_k)
+        values = self.W_v(values).view(bs, -1, self.num_heads, self.d_k)
 
-        # output_concat的形状:(batch_size，查询的个数，num_hiddens)
-        output_concat = transpose_output(output, self.num_heads)
-        return self.W_o(output_concat)
+        output = self.attention(queries, keys, values, mask)
+
+        concat = output.transpose(1, 2).contiguous().view(bs, -1, self.num_hiddens)
+        return self.W_o(concat)
 
 
 class MultiHeadAttention(nn.Module):
@@ -188,11 +124,7 @@ class MultiHeadAttention(nn.Module):
             x,
             x,
             x,
-            (
-                torch.arange(1, x.shape[1] + 1, device=device).repeat(batch_size, 1)
-                if mode == "train"
-                else None
-            ),
+            (generate_square_subsequent_mask(block_size) if mode == "train" else None),
         )
         out = self.dropout(out)
         return out
