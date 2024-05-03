818fe8d2d0572d0707791370aba3ba7b11b39d4e
Ren LIngfeng <e1143935@u.nus.edu>
2024-05-03 15:06:14 +0800

    add special tokens' '

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ import torch
 
 batch_size = 64  # how many independent sequences will we process in parallel?
 block_size = 256  # what is the maximum context length for predictions?
-max_iters = 5000
+max_iters = 1000
 eval_interval = 500
 learning_rate = 3e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -38,11 +38,11 @@ if not os.path.exists(f"tokenizers/{file}.model"):
     tokenizer.train(get_data(), vocab_size=vocab_size)
     tokenizer.save(f"tokenizers/{file}")  # writes tok32k.model and tok32k.vocab
 tokenizer.load(f"tokenizers/{file}.model")  # loads the model back from disk
-print(tokenizer.decode(tokenizer.encode("hello world")) == "hello world")
+print("tokenizer ", tokenizer.decode(tokenizer.encode("hello world")) == "hello world")
 
 
 def encode(t):
-    return tokenizer.encode(t)
+    return tokenizer.encode(t, allowed_special="all")
 
 
 def decode(t):
@@ -50,3 +50,13 @@ def decode(t):
 
 
 mode = "train"
+START = "<|startoftext|>"
+END = "<|endoftext|>"
+special_tokens = [START, END]
+tokenizer.register_special_tokens(
+    {k: vocab_size + i for i, k in enumerate(special_tokens)}
+)
+start_token = torch.tensor(encode(START), dtype=torch.long, device=device)
+end_token = torch.tensor(encode(END), dtype=torch.long, device=device)
+vocab_size = len(tokenizer.vocab) + len(tokenizer.special_tokens)
+print("vocab_size ", vocab_size)
--- gpt_cur4.py
+++ gpt_cur4.py
@@ -19,8 +19,12 @@ def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == "train" else val_data
     ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i : i + block_size] for i in ix])
-    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    x = torch.stack(
+        [torch.cat((start_token, data[i : i + block_size - 1])) for i in ix]
+    )
+    y = torch.stack([torch.cat((data[i : i + block_size - 1], end_token)) for i in ix])
+    # print(x, y)
+    # assert 1 == 2
     x, y = x.to(device), y.to(device)
     return x, y
 
